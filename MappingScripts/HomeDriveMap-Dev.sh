13cef7664e9983379aaa9913d3452dbf28aef914
Jesse Harris <jpharris@c02p41ndg3qn.usc.internal>
2018-05-21 14:25:04 +1000

    Updated group check command to check against usc domain

--- MappingScripts/HomeDriveMap-Dev.sh
+++ MappingScripts/HomeDriveMap-Dev.sh
@@ -38,36 +38,38 @@ function logEntry() {
 logEntry "Starting homemap log version ${version}"
 
 
-    logEntry "User: ${username}"
-    protocol="$4"# This is  the protocol to connect with (afp | smb)
-    logEntry "Protocol: $4"
-    serverName="$5"# This is the afpddress of the server, e.g. my.fileserver.com
-    logEntry "Server: $5"
-    shareName="$6"# This is the     name of the share to mount
-    logEntry "Sharename: $6"
-    group="$7"Sharename# This is the name of the group the user needs to be a member of to mount the share
-    logEntry "Group: $7"
-    # Check that the user is in the necessary group
-    groupCheck=$(dseditgroup -o checkmember -m "${username}" "${group}" | grep -c "yes")
-    if [ "${groupCheck}" -ne 1  ]; then
-            logEntry "${username} not a member of ${group}"
-            exit 1
-        fi
-            # Check if this is first run:
-            if ! test -r "${touchFile}"; then
-                    logEntry "First run, checking to create new home"
-                    if ! test -d "/Volumes/${shareName}"; then
-                                logEntry "${shareName} not mapped yet"
-                                    # root volume not mapped yet
-                                    logEntry "Mapping root volume with applescript..."
-                                    # This is first run, so we are going to mount the root and check if the folder exists.
-                                    mount_script=exists`/usr/bin/osascript > /dev/null << EOT
+logEntry "User: ${username}"
+protocol="$4"	# This is the protocol to connect with (afp | smb)
+logEntry "Protocol: $4"
+serverName="$5"	# This is the address of the server, e.g. my.fileserver.com
+logEntry "Server: $5"
+shareName="$6" # This is the name of the share to mount
+logEntry "Sharename: $6"
+group="$7" # This is the name of the group the user needs to be a member of to mount the share
+logEntry "Group: $7"
+netbios="$8"
+logEntry "Netbios $8"
+# Check that the user is in the necessary group
+groupCheck=$(dseditgroup -o checkmember -n /Active\ Directory/"${netbios}"/All\ Domains -m "${username}" "${group}" | grep -c "yes")
+if [ "${groupCheck}" != "yes" ]; then
+    logEntry "${username} not a member of ${group}"
+    exit 1
+fi
+# Check if this is first run:
+if ! test -r "${touchFile}"; then
+    logEntry "First run, checking to create new home"
+    if ! test -d "/Volumes/${shareName}"; then
+        logEntry "${shareName} not mapped yet"
+        # root volume not mapped yet
+        logEntry "Mapping root volume with applescript..."
+        # This is first run, so we are going to mount the root and check if the folder exists.
+        mount_script=exists`/usr/bin/osascript > /dev/null << EOT
 tell application "Finder" 
 activate
 mount volume "${protocol}://${serverName}/${shareName}"
 end tell
 EOT`
-fi
+        fi
 # First checkingeck if it was succesfully mounted:
     # If sharename contains subdirs, then only the last one is used for the mounted volume
     mountName=$(echo ${shareName##*/}) #bash string operators
