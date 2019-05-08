c5c2008045119084d311c003edcebbda1b5361ab
Jesse Harris <zigford@gmail.com>
2019-05-08 11:07:06 +1000

    simplify parameters for humans

--- MaintScripts/InactiveShutdown.sh
+++ MaintScripts/InactiveShutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
-version=1.0
+version=1.1
 
 # Updated by Jesse Harris - USC
 #
@@ -9,7 +9,7 @@ version=1.0
 # no responsibility for loss or damage
 # caused by this script.
 
-inactiveTimer=${4:-1800} # Default 30 mins of idle
+maxMinsInactive=${4:-30} # Default 30 mins of idle
 enableLog="${5:-no}"
 enableShutdown="${6:-no}"
 logDir=/var/log
@@ -19,33 +19,34 @@ logFile="${logDir}"/InactiveShutdown.log
 function logEntry() {
     msg="$(date "+%d-%m-%y %H:%M"): $1"
     if [[ "${enableLog}" == "yes" ]]; then
-        echo "$msg" | tee -a "${logFile}"
+        echo -e "$msg" | tee -a "${logFile}"
     else
         echo "$msg"
     fi
 }
 
-logEntry "Starting Inactive Shutdown log version ${version}"
-logEntry "InactiveTimer: $((inactiveTimer/60)) mins"
+logEntry "\nStarting Inactive Shutdown log version ${version}"
+logEntry "maxMinsInactive: ${maxMinsInactive} mins"
 
 # Check if any users are logged in:
 
 idleTime=$(ioreg -c IOHIDSystem | awk '/HIDIdleTime/ {print int($NF/1000000000)}')
+idleTime=$((idleTime / 60))
 loggedInUser=$(stat -f '%u %Su' /dev/console | awk '{print $2}')
 
 if [ "${loggedInUser}" != "root" ]; then
     logEntry "User ${loggedInUser} is logged in"
 else
     logEntry "User root is logged in."
-    if [ "${idleTime}" -gt "${inactiveTimer}" ]; then
-        logEntry "Idle time ${idleTime} has exceeded ${inactiveTimer}. Shutting down.."
+    if [ "${idleTime}" -gt "${maxMinsInactive}" ]; then
+        logEntry "Idle time ${idleTime} has exceeded ${maxMinsInactive}. Shutting down.."
         if [[ "${enableShutdown}" == "yes" ]]; then
-            shutdown -h +1
+            shutdown -h +1 &
         else
             logEntry "Demo mode only, not shutting down"
         fi
     else
-        logEntry "Idle time ${idleTime} less then ${inactiveTimer}. Exiting.."
+        logEntry "Idle time ${idleTime} less then ${maxMinsInactive}. Exiting.."
     fi
 fi
 
